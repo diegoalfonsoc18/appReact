fc8753f8f1981ea4cef0d8d5c6ce1093dc13b220
Diego Alfonso <dlalfonso57@hotmail.com>
2023-04-12 14:10:53 -0500

    add form and header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./componentes/Header/Header";
+import Formulario from "./componentes/Formulario/Formulario";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
