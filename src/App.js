4d61027c9cd83629f510a9e6c490c6b74a3e85a0
Diego Alfonso <dlalfonso57@hotmail.com>
2023-04-14 16:06:17 -0500

    function form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./componentes/Header/Header";
 import Formulario from "./componentes/Formulario/Formulario";
 import MiOrg from "./componentes/MiOrg";
 function App() {
+
+  const [mostrarFormulario, actualizarMostrar] = useState(true);
+  
+  const cambiarMostrar = () => {
+  actualizarMostrar(!mostrarFormulario)
+  };
   return (
     <div className="App">
       <Header />
-      <Formulario />
-      <MiOrg />
+      {/* {mostrarFormulario === true? <Formulario /> : <></>} */}
+      {mostrarFormulario && <Formulario />}
+      <MiOrg cambiarMostrar={cambiarMostrar} />
     </div>
   );
 }
--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,20 +1,18 @@
 import { useState } from "react";
 import "./MiOrg.css";
 
-const MiOrg = () => {
-
-
-    const [nombre, actualizarNombre] = useState('Diego');
+const MiOrg = (props) => {
+    console.log(props)
     const [mostrar, actualizarMostrar] = useState(true);
 
     const manejarClick = () => {
-        console.log("Monstrar/Ocultar elemento");
+    console.log("Monstrar/Ocultar elemento", !mostrar);
         actualizarMostrar(!mostrar)
     }
 
     return <section className="orgSection">
         <h3 className="tittle">Mi organización</h3>
-        <img src="/img/add.png" alt="icon" onClick={manejarClick}/>
+        <img src="/img/add.png" alt="icon" onClick={props.cambiarMostrar}/>
     </section>
 }
 
